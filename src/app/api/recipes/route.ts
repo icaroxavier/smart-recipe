43dbf61a904ed52eb053bd15a11e1cdd913d36ab
Ícaro Xavier <icarogabriel999@icloud.com>
2023-05-29 21:53:07 -0300

    teste

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -5,6 +5,7 @@ import { Configuration, OpenAIApi } from 'openai'
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
+
 const openai = new OpenAIApi(configuration)
 
 export async function POST(request: Request) {
@@ -26,6 +27,9 @@ export async function POST(request: Request) {
   })) as any
 
   const responseText = completion.data.choices[0].text.trim()
+
+  console.log('responseText: ', responseText)
+
   const data = JSON.parse(responseText)
   return NextResponse.json(data)
 }
