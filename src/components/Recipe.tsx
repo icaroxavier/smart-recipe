3b2d7672bc84c0710246767774bf2218f0e05d14
Ícaro Xavier <icarogabriel999@icloud.com>
2023-05-30 01:05:13 -0300

    translation done

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,10 +1,13 @@
 import { IRecipe } from '@/types/recipes'
 import { FaClock } from 'react-icons/fa'
+import useTranslation from 'next-translate/useTranslation'
 interface RecipeProps {
   recipe: IRecipe
 }
 
 export function Recipe({ recipe }: RecipeProps) {
+  const { t } = useTranslation('home')
+
   return (
     <div className="flex w-full flex-col rounded-lg bg-white p-5 shadow-xl">
       <div className="flex justify-between">
@@ -16,7 +19,7 @@ export function Recipe({ recipe }: RecipeProps) {
 
       <p className="text-sm  text-zinc-500">{recipe.description}</p>
       <div className="mt-4 flex flex-wrap gap-2 text-sm">
-        <span className="font-medium text-zinc-600">Você utilizará:</span>
+        <span className="font-medium text-zinc-600">{t('recipe.tools')}</span>
         {recipe.tools.map((tool) => (
           <span key={tool} className="flex items-center gap-1 text-zinc-500">
             <div className="h-2 w-2 rounded-full bg-blue-600" />
@@ -24,7 +27,7 @@ export function Recipe({ recipe }: RecipeProps) {
           </span>
         ))}
       </div>
-      <h4 className="mb-1 mt-4 text-zinc-600">Ingredientes necessários: </h4>
+      <h4 className="mb-1 mt-4 text-zinc-600">{t('recipe.ingredients')} </h4>
       <div className="flex flex-col gap-1 text-sm">
         {recipe.ingredients.map((ingredient) => (
           <span
@@ -36,7 +39,7 @@ export function Recipe({ recipe }: RecipeProps) {
           </span>
         ))}
       </div>
-      <h4 className="mb-1 mt-4 text-zinc-600">Passo a Passo: </h4>
+      <h4 className="mb-1 mt-4 text-zinc-600">{t('recipe.step')} </h4>
       <div className="flex flex-col gap-2 text-sm">
         {recipe.steps.map((step) => (
           <div
