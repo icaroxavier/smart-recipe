7329b93009e658b7c62c58257d547bd02c2878e3
Ícaro Xavier <icarogabriel999@icloud.com>
2023-05-29 23:40:16 -0300

    att

--- src/pages/api/recipes.ts
+++ src/pages/api/recipes.ts
@@ -1,38 +1,45 @@
 import { RecipesParams } from '@/types/recipes'
-import { Configuration, OpenAIApi } from 'openai'
 import { NextApiRequest, NextApiResponse } from 'next'
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-
-const openai = new OpenAIApi(configuration)
+import axios from 'axios'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  if (req.method !== 'POST') {
+    return res
+      .status(405)
+      .json({ error: 'Method not allowed, please use POST' })
+  }
   const params: RecipesParams = req.body.params
-
   console.log('params: ', params)
-
   const prompt = `
     Com base no JSON: ${JSON.stringify(params, null, 2)},
     gere pra mim 1 receita no formato JSON. 
     Contendo: "name", "description", "timeToCook", uma lista de "ingredients", uma lista de "tools", uma lista de "steps".
   `
-
-  const completion = (await openai.createCompletion({
+  const payload = {
     model: 'text-davinci-003',
     max_tokens: 2048,
     prompt,
-  })) as any
+  }
 
-  const responseText = completion.data.choices[0].text.trim()
+  const headers = {
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    'Content-Type': 'application/json',
+  }
+
+  const { data } = await axios.post(
+    'https://api.openai.com/v1/completions',
+    payload,
+    { headers },
+  )
+
+  const responseText = data.choices[0].text.trim()
 
   console.log('responseText: ', responseText)
 
-  const data = JSON.parse(responseText)
+  const json = JSON.parse(responseText)
 
-  return res.status(200).json(data)
+  return res.status(200).json(json)
 }
