8ce19cdd7532762ac11a2c7833429626b2d61111
Ícaro Xavier <icarogabriel999@icloud.com>
2023-06-01 18:21:51 -0300

    att

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,10 +1,12 @@
 import PageWrapper from '@/components/PageWrapper'
 import { Recipe } from '@/components/Recipe'
 import { IRecipe } from '@/types/recipes'
+import useTranslation from 'next-translate/useTranslation'
 import { useEffect, useState } from 'react'
 
 export default function HistoryPage() {
   const [recipes, setRecipes] = useState<IRecipe[]>([])
+  const { t } = useTranslation('common')
 
   useEffect(() => {
     const localRecipes = localStorage.getItem(
@@ -17,10 +19,15 @@ export default function HistoryPage() {
 
   return (
     <PageWrapper>
-      <div className="mb-4 mt-4 flex w-full flex-col gap-4">
+      <div className="mb-4 mt-4 flex w-full flex-1 flex-col gap-4">
         {recipes?.map((recipe: IRecipe) => {
           return <Recipe recipe={recipe} key={recipe.name} />
         })}
+        {recipes?.length === 0 && (
+          <h2 className="my-auto text-center text-2xl text-zinc-600">
+            {t('noHistory')}
+          </h2>
+        )}
       </div>
     </PageWrapper>
   )
